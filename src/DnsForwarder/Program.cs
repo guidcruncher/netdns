8acfe0863f74857ca73647680c58a4522909b4b7
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-08-05 18:51:43 +0100

    Scaffolding

--- src/DnsForwarder/Program.cs
+++ src/DnsForwarder/Program.cs
@@ -1,8 +1,3 @@
-using System.Net;
-
-using DnsForwarder.Filtering;
-using DnsForwarder.RuleEngine;
-
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -59,55 +54,8 @@ public class Program
             })
             .ConfigureServices((ctx, services) =>
             {
-                var server = ctx.Configuration.Get<ServerOptions>() ?? new ServerOptions();
-
-                var options = server.Dns;
-
-                // Override listen address via --listen
-                if (ctx.Configuration["ListenOverride"] is string listen)
-                {
-                    var parts = listen.Split(':');
-                    options.Listen.Address = parts[0];
-                    options.Listen.Port = int.Parse(parts[1]);
-                }
-
-                // Override default resolver via --resolver
-                if (ctx.Configuration["ResolverOverride"] is string resolver)
-                {
-                    var parts = resolver.Split(':');
-                    options.DefaultResolver.Address = parts[0];
-                    options.DefaultResolver.Port = parts.Length > 1 ? int.Parse(parts[1]) : 53;
-                }
-
-                services.AddSingleton(options);
-
-                // DNS client + caching
-                services.AddSingleton<StaticDnsClient>();
-
-                services.AddSingleton<IDnsClient>(sp =>
-                {
-                    var opt = sp.GetRequiredService<DnsForwarderOptions>();
-                    var endpoint = new IPEndPoint(
-                        IPAddress.Parse(opt.DefaultResolver.Address),
-                        opt.DefaultResolver.Port);
-
-                    IDnsClient client = new UdpDnsClient(endpoint);
-
-                    if (opt.Caching.Enabled)
-                        client = new CachingDnsClientDecorator(client, opt.Caching.MaxEntries);
-
-                    return client;
-                });
-
-                // Rule engine
-                services.AddSingleton<RuleEngine.RuleEngine>();
-
-                // Hosts loader
-                services.AddSingleton<HostsFileSource>();
-
-                // DNS forwarder service + server
-                services.AddSingleton<DnsForwarderService>();
-                services.AddHostedService<DnsServer>();
+                var bootstrap = new DnsForwarderBootstrap(ctx.Configuration, services);
+                bootstrap.ConfigureServices();
             })
             .Build();
 
@@ -116,65 +64,12 @@ public class Program
         //
         using (var scope = host.Services.CreateScope())
         {
-            var options = scope.ServiceProvider.GetRequiredService<DnsForwarderOptions>();
-            var engine = scope.ServiceProvider.GetRequiredService<RuleEngine.RuleEngine>();
+            var bootstrap = new DnsForwarderBootstrap(
+                scope.ServiceProvider.GetRequiredService<IConfiguration>(),
+                new ServiceCollection() // not used here
+            );
 
-            //
-            // HOSTS FILES
-            //
-            if (options.HostsFiles?.Any() == true)
-            {
-                var hostsPaths = options.HostsFiles
-                    .Select(p => p.StartsWith("file://") ? p[7..] : p);
-
-                var hostsSource = new HostsFileSource(hostsPaths);
-                await engine.AddHostsAsync(hostsSource);
-            }
-
-            //
-            // Helper: choose correct loader based on prefix
-            //
-            IBlocklistSource CreateSource(IEnumerable<string> items)
-            {
-                var fileItems = items
-                    .Where(i => i.StartsWith("file://"))
-                    .Select(i => i.Replace("file://", ""));
-
-                var urlItems = items
-                    .Where(i => !i.StartsWith("file://"));
-
-                if (fileItems.Any() && urlItems.Any())
-                {
-                    return new CompositeBlocklistSource(new IBlocklistSource[]
-                    {
-                        new FileBlocklistSource(fileItems),
-                        new UrlBlocklistSource(urlItems)
-                    });
-                }
-
-                if (fileItems.Any())
-                    return new FileBlocklistSource(fileItems);
-
-                return new UrlBlocklistSource(urlItems);
-            }
-
-            //
-            // BLOCKLISTS
-            //
-            if (options.Blocklists?.Any() == true)
-            {
-                var source = CreateSource(options.Blocklists);
-                await engine.AddListAsync(source, block: true);
-            }
-
-            //
-            // ALLOWLISTS
-            //
-            if (options.Allowlists?.Any() == true)
-            {
-                var source = CreateSource(options.Allowlists);
-                await engine.AddListAsync(source, block: false);
-            }
+            await bootstrap.LoadDataAsync(scope.ServiceProvider);
         }
 
         await host.RunAsync();

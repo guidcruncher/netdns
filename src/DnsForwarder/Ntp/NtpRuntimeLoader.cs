2bfd8fa44587761c6c7df7fab77c8132248bdac3
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-08-06 14:04:48 +0100

    Scaffolding

--- src/DnsForwarder/Ntp/NtpRuntimeLoader.cs
+++ src/DnsForwarder/Ntp/NtpRuntimeLoader.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace DnsForwarder.BootStrap.Ntp;
+namespace DnsForwarder.Ntp.BootStrap;
 
 
 public sealed class NtpRuntimeLoader : IHostedService
--- src/DnsForwarder/Ntp/NtpServerServiceCollectionExtensions.cs
+++ src/DnsForwarder/Ntp/NtpServerServiceCollectionExtensions.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace DnsForwarder.Ntp;
+namespace DnsForwarder.Ntp.Bootstrap;
 
 public static class NtpServerServiceCollectionExtensions
 {

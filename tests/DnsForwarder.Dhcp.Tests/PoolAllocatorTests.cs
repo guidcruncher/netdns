cbf8ad5002d1107297f3b49491e1e55180f31e02
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-08-06 03:41:28 +0100

    Scaffolding

---
+++ tests/DnsForwarder.Dhcp.Tests/PoolAllocatorTests.cs
@@ -0,0 +1,28 @@
+using System.Net;
+using DnsForwarder.Dhcp;
+using FluentAssertions;
+using Xunit;
+
+public class PoolAllocatorTests
+{
+    [Fact]
+    public void Allocate_ShouldReturnFirstFreeIp()
+    {
+        var pool = new CidrPoolAllocator("192.168.10.0/29");
+
+        var ip = pool.Allocate(new IPAddress[0]);
+
+        ip.ToString().Should().Be("192.168.10.1");
+    }
+
+    [Fact]
+    public void AllocationSequence_ShouldEnumerateUsableIps()
+    {
+        var pool = new CidrPoolAllocator("192.168.10.0/29");
+
+        var seq = pool.AllocationSequence(new IPAddress[0]);
+
+        seq.Should().HaveCount(6);
+        seq.First().ToString().Should().Be("192.168.10.1");
+    }
+}
---
+++ tests/DnsForwarder.Dhcp.Tests/ServerEngineTests.cs
@@ -0,0 +1,37 @@
+using System.Net;
+using DnsForwarder.Dhcp;
+using FluentAssertions;
+using Xunit;
+
+public class ServerEngineTests
+{
+    [Fact]
+    public async Task Discover_ShouldProduceOffer()
+    {
+        var store = new InMemoryDhcpLeaseStore();
+        var opts = new DhcpOptions
+        {
+            Enabled = true,
+            ListenAddress = "127.0.0.1",
+            ListenPort = 6767,
+            PoolCidr = "192.168.10.0/29",
+            ServerIdentifier = "192.168.10.1",
+            Router = "192.168.10.1",
+            DnsServer = "1.1.1.1"
+        };
+
+        var logger = TestLogger.Create<DhcpServerEngine>();
+        var engine = new DhcpServerEngine(logger, opts, store);
+
+        var fakeUdp = new FakeUdpClient();
+        var discover = PacketFactory.Discover();
+
+        await fakeUdp.InjectReceive(discover);
+
+        await engine.RunAsync(fakeUdp.CancellationToken);
+
+        fakeUdp.SentPackets.Should().NotBeEmpty();
+        var parsed = DhcpPacketCodec.Parse(fakeUdp.SentPackets.First());
+        parsed.GetMessageType().Should().Be(DhcpMessageType.Offer);
+    }
+}
